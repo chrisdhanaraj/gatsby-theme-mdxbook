4d04aa646ad0edc9205d5f055fb62dbaf24730ab
Chris Dhanaraj <cdhanaraj@netflix.com>
2019-01-19 09:08:01 -0800

    rescaffold the project

--- gatsby-theme-mdxbook/gatsby-config.js
+++ gatsby-theme-mdxbook/gatsby-config.js
@@ -15,9 +15,9 @@ module.exports = {
         // How to resolve each field's value for a supported node type
         resolvers: {
           // For any node of type MarkdownRemark, list how to resolve the fields' values
-          MarkdownRemark: {
+          Mdx: {
             title: node => node.frontmatter.title,
-            keywords: node => node.frontmatter.keywords,
+            content: node => {},
           },
         },
       },
